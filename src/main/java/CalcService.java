12b3a6f61a96e016abcce9b4f9931a9ff345b472
Zina neto <orbit187@yandex.ru>
2025-04-06 11:57:27 +0300

    src corrected

--- src/main/java/CalcService.java
+++ src/main/java/CalcService.java
@@ -2,7 +2,7 @@ public class CalcService {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0; //счётчик месяцев отдыха
         int money = 0; //сумма денег на счету
-        for (int month = 0; month < 12; month++) {
+        for (int month = 1; month < 12; month++) {
             if (money >= threshold) {
                 count++;
                 money = money - expenses - (expenses * 3);
